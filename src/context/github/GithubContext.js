48668ce244bb3a8e9e98192bb9a07a243cbf72ec
Toastalot <115156735+toastalot@users.noreply.github.com>
2022-12-16 18:01:25 +0000

    created github reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,21 +1,34 @@
 import { GITHUB_URL } from "../../env"
-import { createContext, useState } from "react"
+import { createContext, useReducer } from "react"
+import githubReducer from "./githubReducer"
 
 const GithubContext = createContext()
 
 export const GithubProvider = ({ children }) => {
-	const [users, setUsers] = useState([])
-	const [isLoading, setIsLoading] = useState(true)
+	const initialState = {
+		users: [],
+		isLoading: true,
+	}
+
+	const [state, dispatch] = useReducer(githubReducer, initialState)
 
 	const fetchUsers = async () => {
 		const response = await fetch(`${GITHUB_URL}/users`)
 		const data = await response.json()
-		setUsers(data)
-		setIsLoading(false)
+		dispatch({
+			type: "GET_USERS",
+			payload: data,
+		})
 	}
 
 	return (
-		<GithubContext.Provider value={{ users, isLoading, fetchUsers }}>
+		<GithubContext.Provider
+			value={{
+				users: state.users,
+				isLoading: state.isLoading,
+				fetchUsers,
+			}}
+		>
 			{children}
 		</GithubContext.Provider>
 	)
