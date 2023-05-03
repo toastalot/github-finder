f28aa87c0cf3f099b922cc0ca8b6728e67f8a1fd
Toastalot <115156735+toastalot@users.noreply.github.com>
2023-05-03 23:24:39 +0100

    update about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,16 @@
 function About() {
-	return (
-		<div>
-			<h1 className="text-6xl mb-4">Github Finder</h1>
-		</div>
-	)
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <div className="w-2/3 text-center">
+        I built this app to practice React. Use the search bar on the home page
+        to find github profiles by username. Check out my other projects{" "}
+        <a href="https://github.com/toastalot" className="underline">
+          here
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
