8a7b69a8ec156eb366af86b18955eb17aa3fd4d9
Toastalot <115156735+toastalot@users.noreply.github.com>
2023-05-03 23:34:25 +0100

    add heading to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import UserSearch from "../components/users/UserSearch";
 
 function Home() {
   return (
-    <div>
-      <h2></h2>
+    <div className="flex flex-col items-center text-2xl font-bold">
+      <h2 className="mb-8">Search for Github Profiles by Username</h2>
       <UserSearch />
       <UserResults />
     </div>
